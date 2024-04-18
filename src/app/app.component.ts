7a8a1ede9fb85095048a33d15829a08f9f47de58
Lucas Martins <lmartins@tendencias.com.br>
2024-04-18 15:37:04 -0300

    Update app.component.ts to include initial wish items as an array of WishItem objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { WishItem } from './shared/models/wishItem';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items = [
+  items : WishItem[] = [ //estou declarand items como um tipo de array de WishItem
     new WishItem('Learn Angular'),
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
+
   ];
   title = 'wishlist';
 }
