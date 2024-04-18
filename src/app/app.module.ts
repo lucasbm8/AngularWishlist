55f971764ae5e36d3af8e8d2f092cbb7759e6ada
Lucas Martins <lmartins@tendencias.com.br>
2024-04-18 16:06:29 -0300

    Update app.module.ts to import FormsModule and add comments explaining the purpose of the file and its contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
-@NgModule({
-  declarations: [
+//essa arquivo diz tudo que a aplicação precisa pra rodar. 
+
+
+@NgModule({ //há um objeto que define as 
+  declarations: [ //declarações. set de componentes ou directives que pertecem 
+  //a esse modo particular. nesse casso é o appcomponente.
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //imports são disponíveis para os templares dentro desse modulo
+    BrowserModule, 
+    FormsModule, //importando isso agora temos acesso ao ngModel
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//providers são objetos que podem ser injetados a partes desse modulo
+  bootstrap: [AppComponent]//bootstrap
 })
 export class AppModule { }
