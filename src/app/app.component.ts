79c9a7b7acf5ba63b343a16b718988edfa2878e4
Lucas Martins <lmartins@tendencias.com.br>
2024-04-18 14:18:24 -0300

    Add WishItem model and update AppComponent to include initial wish items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WishItem } from './shared/models/wishItem';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items=[
+    new WishItem('Learn Angular', true),
+    new WishItem('get Coffee', true),
+    new WishItem('Secar gelo'),
+  ]
   title = 'wishlist3';
 }
