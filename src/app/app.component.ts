86f32f9dd3c3e1b6496f0b4d8d239f9315b4fbb1
Lucas Martins <lmartins@tendencias.com.br>
2024-04-25 13:54:11 -0300

    Refactor app.component.html to add a column for wish-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { WishItem } from './shared/models/wishItem';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
-
+import events from './shared/services/EventService';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items : WishItem[] = [ //estou declarand items como um tipo de array de WishItem
+  items : WishItem[] = [ //estou declarando items como um tipo de array de WishItem
   new WishItem('Go to the gym'),
   new WishItem('Get groceries', true),
   new WishItem('Get gas'),
 
   ];
+
+  constructor() {
+    events.listen('removeWish', (wish: any) => {
+       //aqui está declarado o que será feito, porém em eventService é onde de fato vou escrever o que eu quero que aconteça quando o usuário tentar remover.
+    })
+  }
 
 filter: any;
 
