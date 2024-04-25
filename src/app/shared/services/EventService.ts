b843c5e72ab35dd25e3a52b5966c083d4d7395ed
Lucas Martins <lmartins@tendencias.com.br>
2024-04-25 13:53:58 -0300

    Add EventService to handle custom events

---
+++ src/app/shared/services/EventService.ts
@@ -0,0 +1,24 @@
+//aqui é onde vai ser de fato implementada a lógica, o que vai ser feito quando o usuário clicar no botão de remover, ou quando ele clicar na checkbox.
+
+import { Observable, Subject } from "rxjs"; //rxjs é uma biblioteca que permite trabalhar com programação reativa, ou seja, trabalhar com eventos, streams de dados, etc. http etc.
+//subject é um tipo de observable que permite emitir valores, ou seja, ele é um tipo de observable que permite emitir valores, e também é um observer, ou seja, ele pode receber valores de outros observables.
+//observable é uma coleção de valores que podem ser emitidos ao longo do tempo.
+
+class EventService{
+    private subjct = new Subject(); //um evento é uma mensagem, 
+
+    emit(eventName: string, payload: any){
+        this.subjct.next({eventName, payload}); //chamamos next que irá pegar o nome e o payload, quaisquer objeto que se inscreve num objeto evento terá um objeto de retorno com essas duas propriedades
+
+    }
+
+    listen(eventName: string, callback: (event: any) => void){
+        this.subjct.asObservable().subscribe((nextObj: any)=>{
+            if(eventName === nextObj.eventName){
+                callback(nextObj.payload);
+            }
+        }) //esse metodo subscribe por padrão aceita uma funcção de callback que irá aceitar qualquer coisa que passamos para o next.
+
+    }
+}
+export default new EventService(); //exportando uma instância de EventService, ou seja, exportando um objeto que é uma instância de EventService.
