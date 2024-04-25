0c7a14f92ef0440f39e953efb41b36df13f7805b
Lucas Martins <lmartins@tendencias.com.br>
2024-04-25 15:11:26 -0300

    Refactor app.component.ts to use WishService for managing wish items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,16 @@ import { Component } from '@angular/core';
 import { WishItem } from './shared/models/wishItem';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { EventService } from './shared/services/EventService';
-
+import { WishService } from './wish.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items : WishItem[] = [ //estou declarando items como um tipo de array de WishItem
-  new WishItem('Go to the gym'),
-  new WishItem('Get groceries', true),
-  new WishItem('Get gas'),
+  items! : WishItem[];
 
-  ];
-
-  constructor(events: EventService) {
+  constructor(events: EventService, private wishService: WishService) {
     events.listen('removeWish', (wish: any) => {
        
        //aqui está declarado o que será feito, porém em eventService é onde de fato vou escrever o que eu quero que aconteça quando o usuário tentar remover.
